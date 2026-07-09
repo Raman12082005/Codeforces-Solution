c5f6d30fdc9989a1bf8b13ae6924e29e94344615
Raman12082005 <rdeep1208y@gmail.com>
2026-07-09 10:14:56 +0530

    add files

---
+++ Crimson_Triples.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        long long ans = 0;
+        for(int b=1;b<=n;b++){
+            long long k = n/b;
+            ans += k*k;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
