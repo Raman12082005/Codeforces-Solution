6094c86b5af3bcb79002d94976427e2a70790a72
Raman12082005 <rdeep1208y@gmail.com>
2026-07-16 22:30:37 +0530

    push code

---
+++ Vasilije_in_Cacak.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k, x;
+        cin>>n>>k>>x;
+        
+        long long minsum = k*(k+1)/2;
+        long long maxsum = k*(2*n-k+1)/2;
+
+        if(x <= maxsum && x >= minsum){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
