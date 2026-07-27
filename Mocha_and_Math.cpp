4d29e2fcd12a535e74dad46c089d788bdc876399
Raman12082005 <rdeep1208y@gmail.com>
2026-07-27 17:36:57 +0530

    push code

---
+++ Mocha_and_Math.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans;
+        cin>>ans;
+        for(int i=1; i<n; i++){
+            int x;
+            cin>>x;
+            ans &= x;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
